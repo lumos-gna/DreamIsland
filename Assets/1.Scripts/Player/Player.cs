ac3185787d1843db0ca6a1c12d00389b426be5b9
ChoiGunWoo1 <gunwoo0813@naver.com>
2025-05-27 17:33:49 +0900

    feat : 임시 아이템 선택기능 추가

--- Assets/1.Scripts/Player/Player.cs
+++ Assets/1.Scripts/Player/Player.cs
@@ -7,24 +7,9 @@ public class Player : MonoBehaviour
 {
     private PlayerState state;
     private PlayerController _playerController;
-    [SerializeField] private GameObject tempitem;
-    [SerializeField] private Transform EquipParent;
 
     private void Awake()
     {
         _playerController = GetComponent<PlayerController>();
-    }
-
-    public void ChoiceItem(GameObject item)// 아이템을 고르는 함수?
-    {
-        Instantiate(item, EquipParent);
-    }
-
-    public void OnChoiceitemInput(InputAction.CallbackContext context)
-    {
-        if(context.phase == InputActionPhase.Started)
-        {
-            ChoiceItem(tempitem);
-        }
     }
 }
