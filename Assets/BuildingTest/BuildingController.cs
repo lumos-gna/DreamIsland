e84d73823f3d24cbafa7b690b35738ab501eaff1
lumos <206481784+lumos-gna@users.noreply.github.com>
2025-05-28 01:46:03 +0900

    feat : 빌딩 시스템 구현 완료

--- Assets/BuildingTest/BuildingController.cs
+++ Assets/BuildingTest/BuildingController.cs
@@ -4,9 +4,6 @@ public class BuildingController : MonoBehaviour
 {
     [SerializeField] private Camera targetCamera;
 
-    [SerializeField] private BuildingObject buildingObjectHPrefab;
-    [SerializeField] private BuildingObject buildingObjectVPrefab;
-    
     [SerializeField] private LayerMask buildableMask;
 
     [SerializeField] private float rayDistance;
@@ -14,84 +11,15 @@ public class BuildingController : MonoBehaviour
     
     private BuildingObject _curBuildingObject;
     
-    
     private bool _isBuildable;
     
-    
-    public float moveSpeed = 5f;
-    public float lookSensitivity = 2f;
-    public float maxYAngle = 80f;
-
-    private float yaw = 0f;
-    private float pitch = 0f;
-    
-
-    private void Start()
+  
+    public void Enable(BuildingObject buildingObjectPrefab)
     {
-        Cursor.lockState = CursorLockMode.Locked;
-        Cursor.visible = false;
+        _curBuildingObject = Instantiate(buildingObjectPrefab);
+        
+        _curBuildingObject.Init();
     }
-    
-    private void Update()
-    {
-        if (Input.GetMouseButton(0))
-        {
-            TryBuild();
-        }
-        
-        if (Input.GetKeyDown(KeyCode.Q))
-        {
-            Enable();
-        }
-        
-        if (Input.GetKeyDown(KeyCode.E))
-        {
-            EnableVertical();
-        }
-        
-        if (Input.GetKeyDown(KeyCode.R))
-        {
-            Vector3 euler = _curBuildingObject.transform.eulerAngles;
-            euler.y += 45f;
-            _curBuildingObject.transform.eulerAngles = euler;
-        }
-
-       
-        
-        UpdateBuildingObject();
-
-        
-        float mouseX = Input.GetAxis("Mouse X") * lookSensitivity;
-        float mouseY = Input.GetAxis("Mouse Y") * lookSensitivity;
-
-        yaw += mouseX;
-        pitch -= mouseY;
-        pitch = Mathf.Clamp(pitch, -maxYAngle, maxYAngle);
-
-        targetCamera.transform.eulerAngles = new Vector3(pitch, yaw, 0f);
-
-        float moveX = Input.GetAxis("Horizontal");
-        float moveZ = Input.GetAxis("Vertical");  
-
-        Vector3 move = targetCamera.transform.right * moveX + targetCamera.transform.forward * moveZ;
-        targetCamera.transform.position += move * moveSpeed * Time.deltaTime;
-    }
-
-
-    public void Enable()
-    {
-        _curBuildingObject = Instantiate(buildingObjectHPrefab);
-        
-        _curBuildingObject.InitToBuilding();
-    }
-    
-    public void EnableVertical()
-    {
-        _curBuildingObject = Instantiate(buildingObjectVPrefab);
-        
-        _curBuildingObject.InitToBuilding();
-    }
-    
     
     public void Disable() =>  Destroy(_curBuildingObject.gameObject);
     
@@ -126,7 +54,7 @@ public class BuildingController : MonoBehaviour
         {
             _curBuildingObject.transform.position = hit.point;
 
-            TrySnap(hit);
+            Snap(hit);
 
             _isBuildable = true;
         }
@@ -135,33 +63,23 @@ public class BuildingController : MonoBehaviour
     }
 
 
-    bool TrySnap(RaycastHit hit)
+    void Snap(RaycastHit hit)
     {
         if (hit.collider.TryGetComponent(out BuildingObject targetObject))
         {
-            if (targetObject.IsSnappable() &&_curBuildingObject.IsSnappable() )
+            if (targetObject.IsSnappable &&_curBuildingObject.IsSnappable)
             {
-                BuildingSnapPoint targetSnapPoint = targetObject.GetClosestSnapPointToHit(hit.point);
+                BuildingSnapPoint targetSnapPoint = targetObject.GetSnapPointClosestHit(hit.point);
 
-                //BuildingSnapPoint curSnapPoint = _curBuildingObject.GetClosestSnapPointToHit(targetObject.transform.position);
-                
-                BuildingSnapPoint curSnapPoint = _curBuildingObject.GetClosestSnapPointToSnapPoint(targetSnapPoint);
+                BuildingSnapPoint curSnapPoint = _curBuildingObject.GetSnapPointClosestTargetPoint(targetSnapPoint);
 
                 if (curSnapPoint != null)
                 {
                     Vector3 offset = targetSnapPoint.transform.position - curSnapPoint.transform.position;
                     
                     _curBuildingObject.transform.position += offset;
-                    
-                    Debug.Log(targetSnapPoint.gameObject.name);
-                    //Debug.Log(curSnapPoint.gameObject.name);
-                    
-                    return true;
                 }
             }
         }
-
-        return false;
     }
-    
 }
--- Assets/BuildingTest/BuildingObject.cs
+++ Assets/BuildingTest/BuildingObject.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 
 public class BuildingObject : MonoBehaviour
 {
+   public bool IsSnappable => snapPoints.Length > 0;
+   
    [SerializeField] private MeshRenderer meshRenderer;
    [SerializeField] private Material builtMaterial;
    [SerializeField] private Material fadeMaterial;
@@ -13,15 +15,16 @@ public class BuildingObject : MonoBehaviour
    [SerializeField] private BuildingSnapPoint[] snapPoints;
 
 
-
-   public void InitToBuilding()
+   public void Init()
    {
       meshRenderer.material = fadeMaterial;
       
       gameObject.layer = 2;
    }
    
+   
    public void UpdateToBuildingState(bool isBuildable) => meshRenderer.enabled = isBuildable;
+   
 
    public void Built()
    {
@@ -29,13 +32,10 @@ public class BuildingObject : MonoBehaviour
       
       gameObject.layer =  0;
    }
-
-   public bool IsSnappable() => snapPoints.Length > 0;
-  
-
    
 
-   public BuildingSnapPoint GetClosestSnapPointToHit(Vector3 hitPoint)
+   
+   public BuildingSnapPoint GetSnapPointClosestHit(Vector3 hitPoint)
    {
       BuildingSnapPoint snapPoint = null;
 
@@ -56,20 +56,22 @@ public class BuildingObject : MonoBehaviour
       return snapPoint;
    }
 
-   public BuildingSnapPoint GetClosestSnapPointToSnapPoint(BuildingSnapPoint targetSnapPoint)
+   
+   
+   public BuildingSnapPoint GetSnapPointClosestTargetPoint(BuildingSnapPoint targetPoint)
    {
       BuildingSnapPoint tempSnapPoint = null;
 
-      float tempDist = float.MaxValue;
+      float tempDist = float.MinValue;
 
       foreach (var item in snapPoints)
       {
          if (item.Axis == BuildingSnapPoint.SnapAxis.All ||
-             item.Axis ==  targetSnapPoint.Axis)
+             item.Axis ==  targetPoint.Axis)
          {
-            float compareDist = Vector3.Distance(item.transform.position, targetSnapPoint.transform.position);
+            float compareDist = Vector3.Distance(item.transform.localPosition, targetPoint.transform.localPosition);
             
-            if (compareDist < tempDist)
+            if (compareDist > tempDist)
             {
                tempDist = compareDist;
 
