2192d4f81d531284813c027c2b6521d4392b629e
JangHoon Ban <janghoon333@naver.com>
2025-05-27 20:52:44 +0900

    feat : Item 클래스 생성, Interface 생성

--- Assets/1.Scripts/Player/Player.cs
+++ Assets/1.Scripts/Player/Player.cs
@@ -1,15 +1,15 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.InputSystem;
+﻿//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
+//using UnityEngine.InputSystem;
 
-public class Player : MonoBehaviour
-{
-    private PlayerState state;
-    private PlayerController _playerController;
+//public class Player : MonoBehaviour
+//{
+//    private PlayerState state;
+//    private PlayerController _playerController;
 
-    private void Awake()
-    {
-        _playerController = GetComponent<PlayerController>();
-    }
-}
+//    private void Awake()
+//    {
+//        _playerController = GetComponent<PlayerController>();
+//    }
+//}
---
+++ Assets/1.Scripts/ScriptableObject/ItemData.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum ItemType
+{
+    Weapon,
+    Resource,
+    Consumable
+}
+
+public enum ConsumableType
+{
+    Health,     // 체력
+    Thirsty,    // 목마름
+    Stamina,    // 스태미나
+}
+
+[SerializeField]
+public class ConsumableItem
+{
+    public ConsumableType type;
+    public float value;
+}
+
+[CreateAssetMenu(fileName = "Item_", menuName = "New Item")]
+public class ItemData : ScriptableObject
+{
+    [Header("Information")]
+    public ItemType type;
+    public string displayName;
+    public string description;
+    public float value;
+    public GameObject dropItemPrefab;
+
+    [Header("Stack")]
+    public bool canStack;
+    public int maxStackCount;
+
+    [Header("Consumable data")]
+    public ConsumableItem[] consumables;
+
+    [Header("Equip")]
+    public GameObject equipPrefab;
+}
