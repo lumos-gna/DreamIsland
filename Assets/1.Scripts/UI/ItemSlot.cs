9adb93b3e3626c2ab128f679bba8755ffad17b64
JangHoon Ban <janghoon333@naver.com>
2025-05-28 16:04:21 +0900

    feat : 인벤토리 아이템 드래그앤드롭 기능 구현중

--- Assets/1.Scripts/UI/ItemSlot.cs
+++ Assets/1.Scripts/UI/ItemSlot.cs
@@ -6,7 +6,7 @@ using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
-public class ItemSlot : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+public class ItemSlot : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IBeginDragHandler, IDragHandler, IEndDragHandler
 {
     public ItemData item;
 
@@ -19,6 +19,11 @@ public class ItemSlot : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
     public bool equiped;
     public int quantity;
 
+    private GameObject dragIcon;
+    private RectTransform dragIconRect;
+
+
+    // 슬롯 세팅
     public void SetSlot()
     {
         icon.gameObject.SetActive(true);
@@ -26,6 +31,7 @@ public class ItemSlot : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
         quantityText.text = quantity > 1 ? quantity.ToString() : string.Empty;
     }
 
+    // 슬롯 초기화
     public void ClearSlot()
     {
         item = null;
@@ -33,6 +39,7 @@ public class ItemSlot : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
         quantityText.text = string.Empty;
     }
 
+    // 마우스 포인터가 아이콘 위에 있을때 감지
     public void OnPointerEnter(PointerEventData eventData)
     {
         if (item != null)
@@ -42,11 +49,34 @@ public class ItemSlot : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
         }
     }
 
+    // 마우스 포인터가 아이콘에서 벗어났을때
     public void OnPointerExit(PointerEventData eventData)
     {
         inventory.summaryBox.SetActive(false);
         inventory.ClearSelectedItemWindow();
     }
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        if (item == null) return;
+
+        // 드래그할 아이콘 생성
+        dragIcon = new GameObject("DragIcon", typeof(RectTransform), typeof(CanvasGroup), typeof(Image));
+        dragIcon.transform.SetParent(inventory.transform, false);   // UIInventory 하위에 생성
+        dragIcon.transform.SetAsLastSibling();  // 맨 앞에서 렌더링
+
+        dragIconRect = dragIcon.GetComponent<RectTransform>();
+        dragIconRect.sizeDelta = icon.rectTransform.sizeDelta;
+
+        var image = dragIcon.GetComponent<Image>();
+        image.sprite = icon.sprite;
+        image.raycastTarget = false;
+
+        // 투명도 설정
+        var group = dragIcon.GetComponent<CanvasGroup>();
+        group.blocksRaycasts = false;
+    }
+
 
     public void OnClickButton()
     {
