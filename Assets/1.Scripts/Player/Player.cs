1197e37d4d8e8a53597a3945e0904de6df48bee7
ChoiGunWoo1 <gunwoo0813@naver.com>
2025-05-28 12:52:02 +0900

    feat : 간단한 애니메이션 추가 / 플레이어 상태 추가

--- Assets/1.Scripts/Player/Player.cs
+++ Assets/1.Scripts/Player/Player.cs
@@ -7,9 +7,13 @@ public class Player : MonoBehaviour
 {
     private PlayerState state;
     private PlayerController _playerController;
+    private PlayerCondition _playerCondition;
+    private ItemEquip _itemEquip;
 
     private void Awake()
     {
         _playerController = GetComponent<PlayerController>();
+        _playerCondition = GetComponent<PlayerCondition>();
+        _itemEquip = GetComponent<ItemEquip>();
     }
 }
---
+++ Assets/1.Scripts/Player/PlayerCondition.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCondition : MonoBehaviour
+{
+    [SerializeField] private float health;
+    [SerializeField] private float water;
+    [SerializeField] private float stamina;
+
+    private float waterDecreaseperFrame = 0.001f;
+    private float thirstyDecreaseHealth = 0.1f;
+    private float minf = 0f;
+    private float maxf = 100f;
+
+
+    private void Start()
+    {
+        health = 100f;
+        water = 100f;
+        stamina = 100f;
+    }
+
+    private void Update()
+    {
+        WaterChange(-waterDecreaseperFrame); // 목마름 계속 감소
+        if (water == minf) // 목마름이 0이면, 체력 감소
+        {
+            HealthChange(-thirstyDecreaseHealth);
+        }
+    }
+
+    public void HealthChange(float change) // 각 condition 변화 적용 함수
+    {
+        health = Mathf.Clamp(health + change, minf, maxf);
+    }
+    public void WaterChange(float change)
+    {
+        water = Mathf.Clamp(water + change, minf, maxf);
+    }
+    public void StaminaChange(float change)
+    {
+        stamina = Mathf.Clamp(stamina + change, minf, maxf);
+    }
+
+
+}
