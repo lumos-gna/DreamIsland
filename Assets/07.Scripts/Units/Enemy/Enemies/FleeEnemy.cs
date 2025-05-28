6fee68fda2d7f49afc6bf5b81d38e313ee25099f
lumos <206481784+lumos-gna@users.noreply.github.com>
2025-05-28 23:50:38 +0900

    Revert "Merge branch 'develop' of https://github.com/lumos-gna/PaperIsland into develop"
    This reverts commit 670a087caf985177e6434282451b485a9b87a8f8, reversing
    changes made to 6c5e730e1136498b6746030a8f77648ffb39fb5b.

--- Assets/07.Scripts/Units/Enemy/Enemies/FleeEnemy.cs
+++ Assets/07.Scripts/Units/Enemy/Enemies/FleeEnemy.cs
@@ -7,26 +7,6 @@ public class FleeEnemy : BaseEnemy
     [SerializeField] FleeEnemyStats _fleeStats;
     public override FleeEnemyStats FleeEnemyStats => _fleeStats;
 
-    protected override void Awake()
-    {
-        base.Awake();
-        // FleeEnemyStats.WanderCenter가 비어 있으면 Player의 Transform으로 채워주기
-        if (_fleeStats.WanderCenter == null)
-        {
-            GameObject playerObj = GetPlayer();
-            if (playerObj != null)
-            {
-                _fleeStats.WanderCenter = playerObj.transform;
-            }
-            else
-            {
-                var found = GameObject.FindWithTag("Player");
-                if (found != null)
-                    _fleeStats.WanderCenter = found.transform;
-            }
-        }
-    }
-
     private void OnDrawGizmosSelected()
     {
         Gizmos.color = Color.cyan;
