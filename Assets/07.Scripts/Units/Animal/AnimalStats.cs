d851754716fecdfa16b4072d715772c328e14d29
lumos <206481784+lumos-gna@users.noreply.github.com>
2025-05-28 17:16:52 +0900

    rename : 디렉토리정리

---
+++ Assets/07.Scripts/Units/Animal/AnimalStats.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class AnimalStats
+{
+    // 동물 Enemy 추가 스텟
+    public Transform WanderCenter;
+    public float WanderRadius = 10f;
+    public float FleeDistance = 5f;
+    public float FleeDuration = 2f;
+}
---
+++ Assets/07.Scripts/Units/Animal/Penguin.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Penguin : BaseEnemy
+{
+    [SerializeField] private AnimalStats _animalStats;
+    public override AnimalStats AnimalStats => _animalStats;
+
+    // 움직이는 전체 범위 확인
+    private void OnDrawGizmosSelected()
+    {
+        if (_animalStats != null)
+        {
+            Gizmos.color = Color.red;
+            //Gizmos.DrawWireSphere(_animalStats.WanderCenter.position, _animalStats.WanderRadius);
+            Gizmos.DrawWireSphere(transform.position, Stats.DetectDistance);
+        }
+    }
+}
