ef42eb560170e4e9d6b480d99bf789d6d5b22266
lumos <206481784+lumos-gna@users.noreply.github.com>
2025-05-30 18:06:30 +0900

    feat : projectile 클래스 추가

---
+++ Assets/Temp/TempItem/Projectile.cs
@@ -0,0 +1,48 @@
+using System;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour, IPoolable
+{
+    [SerializeField] private Rigidbody rigid;
+
+    [SerializeField] private float maxLifeTime;
+    
+
+    private Projectile _originPrefab;
+    
+    private float _curLifeTime;
+
+
+    public void Fire(Projectile originPrefab, Vector3 point,  Vector3 dir, float force)
+    {
+        _originPrefab = originPrefab;
+        
+        transform.position = point;
+
+        transform.forward = dir;
+
+        rigid.AddForce(force * dir, ForceMode.Impulse);
+    }
+
+    private void Update()
+    {
+        _curLifeTime += Time.deltaTime;
+
+        if (_curLifeTime >= maxLifeTime)
+        {
+            PoolManager.Instance.GetPool(_originPrefab).Despawn(this);
+        }
+    }
+
+
+    public void OnSpawn()
+    {
+        _curLifeTime = 0;
+        gameObject.SetActive(true);
+    }
+
+    public void OnDespawn()
+    {
+        gameObject.SetActive(false);
+    }
+}
