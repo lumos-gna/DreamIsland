5bd31accbc68bca554b8eb0dea4c060c4a9921be
lsh000219 <147456856+lsh000219@users.noreply.github.com>
2025-05-30 15:03:14 +0900

    feat - 펭귄 에셋 추가

--- Assets/07.Scripts/Units/Npc/NpcManager.cs
+++ Assets/07.Scripts/Units/Npc/NpcManager.cs
@@ -6,6 +6,10 @@ public class NpcManager : MonoBehaviour
 {
 
     public NPCUIController npcController;
+    private GameObject _player;
+    public Vector3 offset;
+    public float followSpeed; // 따라오는 속도
+    public float maxDistance; // 최대 거리 제한
 
     private void OnCollisionEnter(Collision other)
     {
@@ -22,5 +26,29 @@ public class NpcManager : MonoBehaviour
 
             npcController.OnOff();
         }
+    }
+    
+    
+
+    void Start()
+    {
+        _player = GameObject.Find("Player");
+    }
+    
+    
+
+    void Update()
+    {
+        Vector3 targetPosition = _player.transform.position + offset;
+        float distance = Vector3.Distance(transform.position, targetPosition);
+
+        if (distance > maxDistance)
+        {
+            // 거리가 너무 멀면, 요정을 부드럽게 이동시켜 따라오게 함
+            transform.position = Vector3.Lerp(transform.position, targetPosition, followSpeed * Time.deltaTime);
+        }
+        
+        transform.LookAt(_player.transform);
+
     }
 }
