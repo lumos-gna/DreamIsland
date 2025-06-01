2c666ad23d81301155b3201e3e5110b55defe4d9
lumos <206481784+lumos-gna@users.noreply.github.com>
2025-06-02 01:57:47 +0900

    fix : 상호작용, 제단 오류 수정

--- Assets/07.Scripts/GameManager.cs
+++ Assets/07.Scripts/GameManager.cs
@@ -6,13 +6,24 @@ public class GameManager : Singleton<GameManager>
 {
     public bool IsLockedCursor { get; private set; }
 
-    public Inventory Inventory { get; private set; }
+    public Inventory Inventory
+    {
+        get
+        {
+            if (_inventory == null)
+            {
+                _inventory = new Inventory(itemSlotCount: 21, handleSlotCount: 7);
+            }
+
+            return _inventory;
+        }
+    }
 
     private PlayerCondition _playerCondition;
+    private Inventory _inventory;
 
     private void Awake()
     {
-        Inventory = new Inventory(itemSlotCount: 21, handleSlotCount: 7);
         IsLockedCursor = true;
         Cursor.lockState = CursorLockMode.Locked;
     }
@@ -20,7 +31,6 @@ public class GameManager : Singleton<GameManager>
     private void Start()
     {
         SceneManager.sceneLoaded += OnSceneLoaded;
-
         InitializeUI();
     }
 
