125efd9b5874bc16affc634d64fc0f0caf5b05de
ChoiGunWoo1 <gunwoo0813@naver.com>
2025-06-02 12:27:18 +0900

    feat : 인트로 화면 추가

--- Assets/07.Scripts/UI/StartScene/StartTitleFadeIn.cs
+++ Assets/07.Scripts/UI/StartScene/StartTitleFadeIn.cs
@@ -4,10 +4,14 @@ using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+using UnityEngine.Video;
 
 public class StartTitleFadeIn : MonoBehaviour
 {
-
+    [SerializeField] private VideoPlayer videoPlayer;
+    [SerializeField] private Texture2D fallbackImage;
+    [SerializeField] private GameObject BackGround;
+    [SerializeField] private RenderTexture renderTexture;
     [SerializeField] private GameObject title;
     [SerializeField] private GameObject Btn;
     [SerializeField] private GameObject txt;
@@ -16,11 +20,17 @@ public class StartTitleFadeIn : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        StartCoroutine(FadeIn());
+        videoPlayer.loopPointReached += OnVideoFinished;
+
+        // 비디오 출력 설정
+        videoPlayer.targetTexture = renderTexture;
+        BackGround.GetComponent<RawImage>().texture = renderTexture;
+        videoPlayer.Play();
     }
 
     IEnumerator FadeIn()
     {
+        txt.SetActive(true);
         float fadetime = 0f;
         float endtime = 2.5f;
         while(fadetime <= endtime)
@@ -40,6 +50,11 @@ public class StartTitleFadeIn : MonoBehaviour
         yield break;
     }
 
+    void OnVideoFinished(VideoPlayer vp)
+    {
+        BackGround.GetComponent<RawImage>().texture = fallbackImage;
+        StartCoroutine(FadeIn());
+    }
     public void GameStart()
     {
         SceneManager.LoadScene(mainscenename);
